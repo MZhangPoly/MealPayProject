ef2e5525387d8dec1d3aa83a7cb7d94006e27f62
Michael Zhang <mzhang26@students.polytechnic.org>
2024-02-08 15:09:12 -0800

    added methods to account

--- Account.java
+++ Account.java
@@ -8,7 +8,32 @@ public class Account() {
     public Account(String name) {
         this.name = name;
         balance = 0;
+
         id = accountsCreated;
         accountsCreated++;
+    }
+
+    public void Deposit(int deposit) {
+        balance += deposit;
+    }  
+
+    public void Charge(int charge) {
+        balance -= charge;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return "Account name: " + name + " - balance: " + balance + " - unique id: " + id;
     }
 }
