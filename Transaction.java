6498c9adbae5c96e350be98ff72a992d8037aa06
Michael Zhang <mzhang26@students.polytechnic.org>
2024-02-12 10:05:20 -0800

    added transaction getter functions

--- Transaction.java
+++ Transaction.java
@@ -1,9 +1,25 @@
 public class Transaction {
     private Account account;
     private double balanceChange;
+    private Date date;
 
-    public Transaction(Account account, double balanceChange) {
+    public Transaction(Account account, double balanceChange, Date date) {
         this.account = account;
         this.balanceChange = balanceChange;
+        this.date = date;
+
+        AccountHandler.addTransactionToLog();
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public double getBalanceChange() {
+        return balanceChange;
+    }
+
+    public Account getAccount() {
+        return account;
     }
 }
