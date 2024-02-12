2c0d4f555214b34a93ecd21c92cba0d365fb945b
Michael Zhang <mzhang26@students.polytechnic.org>
2024-02-12 12:15:59 -0800

    fixed format so its m/d/y

--- Date.java
+++ Date.java
@@ -3,14 +3,14 @@ public class Date {
     private int month;
     private int day;
 
-    public Date(int year, int month, int day) {
-        this.year = year;
+    public Date(int month, int day, int year) {
         this.month = month;
         this.day = day;
+        this.year = year;
     }
 
     public int[] getDateArray() {
-        return new int[] {year, month, day};
+        return new int[] {month, day, year};
     }
 
     public String toString() {
